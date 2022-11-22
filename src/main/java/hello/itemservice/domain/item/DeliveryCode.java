6074baad5896956f3bd8a60c8099fa0520bcd4c1
Kai Kim <juhyeon1114@gmail.com>
2022-11-22 15:07:59 +0900

    log : 셀렉트 박스

--- src/main/java/hello/itemservice/domain/item/DeliveryCode.java
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -1,6 +1,16 @@
 package hello.itemservice.domain.item;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DeliveryCode {
     private String code;
     private String displayName;
+
+    public DeliveryCode(String code, String displayName) {
+        this.code = code;
+        this.displayName = displayName;
+    }
 }
